5fde405460bb6107e3251cbd7b2a7e5f6c19f401
Manuel De Orta <manueldeortac@gmail.com>
2021-11-23 08:38:32 -0500

    Configuracion de alternativa para puerto https

--- src/core/squamas/http/HttpService.ts
+++ src/core/squamas/http/HttpService.ts
@@ -7,8 +7,9 @@ import fs from "fs";
 import ip from "ip";
 import * as Socket from "socket.io";
 
-let $http:Http;
-let $ioServer:IoServer;
+let $http: Http;
+let $ioServer: IoServer;
+
 export class HttpService extends AbstractService {
 
 
@@ -16,21 +17,24 @@ export class HttpService extends AbstractService {
         $http = express();
         let config: any = kernel.getConfig('http');
         let publicDir = kernel.projectDir + '/public';
-        if(config.publicDir){
-            publicDir = path.join( kernel.projectDir , config.publicDir);
+        if (config.publicDir) {
+            publicDir = path.join(kernel.projectDir, config.publicDir);
         }
         $http.use(express.static(publicDir));
         console.log(" Using for public directory ->", publicDir);
 
-        let onReady:Function;
+        let onReady: Function;
+        let port: number;
+        if (Array.isArray(config['port'])) {
+            port = config['port'][0];
+        } else port = config['port'];
 
-        let port:number = config['port'];
-        let ssl:any = config['ssl'];
+        let ssl: any = config['ssl'];
         if (ssl) {
             let options = {key: fs.readFileSync(ssl.key), cert: fs.readFileSync(ssl.cert)};
             let server = require('https').createServer(options, $http);
             $ioServer = new Socket.Server(server);
-            onReady = ()=>{
+            onReady = () => {
                 server.listen(port, () => {
                     console.log("\x1b[32m", '');
                     console.log(' +----------------------------------------------------------+');
@@ -40,14 +44,32 @@ export class HttpService extends AbstractService {
                     console.log(string);
                     console.log(' +----------------------------------------------------------+\n');
                     console.log("\x1b[0m", '');
+
+                    if (config['port']) {
+                        let port: number;
+                        if (Array.isArray(config['port'])) {
+                            port = config['port'][1];
+                        } else port = config['port'] + 1;
+                        let options = {};
+                        let server = require('http').createServer(options, $http);
+                        server.listen(port, () => {
+                            console.log("\x1b[32m", '');
+                            console.log(' +------------------------------------------------+');
+                            let string = ' | Listening on ' + ip.address() + ':' + port;
+                            while (string.length < 50) string += ' ';
+                            string += '|';
+                            console.log(string);
+                            console.log(' +------------------------------------------------+\n');
+                            console.log("\x1b[0m", '');
+                        });
+                    }
                 });
             }
-        }
-        else {
+        } else {
             let options = {};
             let server = require('http').createServer(options, $http);
             $ioServer = new Socket.Server(server);
-            onReady = ()=>{
+            onReady = () => {
 
                 server.listen(port, () => {
                     console.log("\x1b[32m", '');
@@ -68,15 +90,15 @@ export class HttpService extends AbstractService {
 
             let $connScope = {};
 
-            $this._socketOnFunction.forEach((listener:any) => {
+            $this._socketOnFunction.forEach((listener: any) => {
                 if (listener.name === '$connection') {
                     listener.fn($socket, {}, $connScope);
                 }
             });
 
-            $this._socketOnFunction.forEach((listener:any) => {
+            $this._socketOnFunction.forEach((listener: any) => {
                 if (listener.name !== '$disconnect' && listener.name !== '$connection')
-                    $socket.on(listener.name, function (data: any, callback:Function) {
+                    $socket.on(listener.name, function (data: any, callback: Function) {
                         if (data !== '' && data !== undefined && data !== null && typeof data === 'string') data = JSON.parse(decodeURIComponent(escape(atob(data))));
                         listener.fn($socket, data, $connScope, callback);
                     });
@@ -92,13 +114,14 @@ export class HttpService extends AbstractService {
         kernel.onReady(onReady);
     }
 
-    public finalize(instances:any): void {
+    public finalize(instances: any): void {
     }
 
     public instances(services: any): any {
-        return {$http, $ioServer, $cookies: (name:string) => {
+        return {
+            $http, $ioServer, $cookies: (name: string) => {
                 let {$request} = services;
-                let cookiesMap:any = {};
+                let cookiesMap: any = {};
                 if (typeof $request.headers.cookie === 'string')
                     $request.headers.cookie.split(';').forEach((stringCookie: string) => {
                         let [c, v] = stringCookie.split('=');
@@ -110,15 +133,16 @@ export class HttpService extends AbstractService {
             }, $redirect: (url: string) => {
                 let {$send, $request} = services;
                 if (!/(http:\\\\)|(https:\\\\)/.test(url)) {
-                    url = $request.protocol+'://'+$request.get('host') + '/' +url;
+                    url = $request.protocol + '://' + $request.get('host') + '/' + url;
                 }
-                $send((response:Response) => {
+                $send((response: Response) => {
                     response.writeHead(307,
                         {Location: url}
                     );
                     response.end();
                 });
-            }};
+            }
+        };
     }
 
 
@@ -141,9 +165,10 @@ export class HttpService extends AbstractService {
                 break;
         }
     }
-    private _socketOnFunction:Array<any> = []
 
-    public addSocketOn(name: string, fn: Function){
+    private _socketOnFunction: Array<any> = []
+
+    public addSocketOn(name: string, fn: Function) {
         this._socketOnFunction.push({name, fn});
     }
 
