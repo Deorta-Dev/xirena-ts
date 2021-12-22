634fa5d8e87878370b6d1c56b3a68329974a2bfd
Manuel De Orta <manueldeortac@gmail.com>
2021-12-22 14:15:40 -0500

    Update socket created

--- src/core/squamas/http/HttpService.ts
+++ src/core/squamas/http/HttpService.ts
@@ -19,6 +19,7 @@ export class HttpService extends AbstractService {
         if (config.publicDir) {
             publicDir = path.join(kernel.projectDir, config.publicDir);
         }
+        $http.use(express.json({type: '*/*'}));
         $http.use(express.static(publicDir));
         console.log(" Using for public directory ->", publicDir);
 
@@ -43,11 +44,7 @@ export class HttpService extends AbstractService {
         if (ssl) {
             let options = {key: fs.readFileSync(ssl.key), cert: fs.readFileSync(ssl.cert)};
             let server = require('https').createServer(options, $http);
-            $ioServer = require("socket.io")(server,  {
-                cors: {
-                    origin: '*',
-                }
-            });
+            $ioServer = require("socket.io")(server);
             onReady = () => {
                 server.listen(port, () => {
                     console.log("\x1b[32m", '');
@@ -73,11 +70,7 @@ export class HttpService extends AbstractService {
         } else {
             let options = {};
             let server = require('http').createServer(options, $http);
-            $ioServer = require("socket.io")(server,  {
-                cors: {
-                    origin: '*',
-                }
-            });
+            $ioServer = require("socket.io")(server);
             onReady = () => {
 
                 server.listen(port, normalListener);
