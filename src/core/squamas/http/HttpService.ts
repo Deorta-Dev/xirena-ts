980464cf580a35b4b0ed76cf277014dc58880724
Manuel De Orta <manueldeortac@gmail.com>
2021-12-21 19:25:47 -0500

    Update socket created

--- src/core/squamas/http/HttpService.ts
+++ src/core/squamas/http/HttpService.ts
@@ -43,7 +43,11 @@ export class HttpService extends AbstractService {
         if (ssl) {
             let options = {key: fs.readFileSync(ssl.key), cert: fs.readFileSync(ssl.cert)};
             let server = require('https').createServer(options, $http);
-            $ioServer = new Socket.Server(server);
+            $ioServer = require("socket.io")(server,  {
+                cors: {
+                    origin: '*',
+                }
+            });
             onReady = () => {
                 server.listen(port, () => {
                     console.log("\x1b[32m", '');
@@ -66,12 +70,16 @@ export class HttpService extends AbstractService {
                     }
                 });
             }
-        }
-        else {
+        } else {
             let options = {};
             let server = require('http').createServer(options, $http);
-            $ioServer = new Socket.Server(server);
+            $ioServer = require("socket.io")(server,  {
+                cors: {
+                    origin: '*',
+                }
+            });
             onReady = () => {
+
                 server.listen(port, normalListener);
             }
 
@@ -79,7 +87,7 @@ export class HttpService extends AbstractService {
 
         let $this = this;
         $ioServer.on('connection', ($socket: Socket.Socket) => {
-            console.log(new Date(), "new connection on socket");
+
             let $connScope = {};
 
             $this._socketOnFunction.forEach((listener: any) => {
